2da09230166f2c7592bed27b64414830ae3004c1
Corzord <jrcgoh001@mymail.sim.edu.sg>
2023-02-11 09:05:48 +0800

    start

---
+++ QueueSim.java
@@ -0,0 +1,14 @@
+//Queue Simulator
+
+import java.util.*;
+
+class QueueSim {
+    public static void main (String[] args){
+        //ask user to input total minutes to run
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please enter the total minutes to run: ");
+        int totalMinutes = input.nextInt();
+
+    }
+
+}
